5fb879da32d01e06eef31037491599b34446be45
Guillem Prada Torres <gprada-t@car10s1.42barcelona.com>
2024-07-21 13:05:00 +0200

    main to complain and show everything works as expected

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: gprada-t <gprada-t@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/21 10:31:04 by gprada-t          #+#    #+#             */
-/*   Updated: 2024/07/21 12:45:07 by gprada-t         ###   ########.fr       */
+/*   Updated: 2024/07/21 12:58:13 by gprada-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ ClapTrap::ClapTrap(std::string &name)
 
 ClapTrap::ClapTrap(const ClapTrap &src)
 {
+	std::cout << "Copy constructor called." << std::endl;
 	*this = src;
 }
 
@@ -33,16 +34,22 @@ ClapTrap::~ClapTrap()
 
 ClapTrap	&ClapTrap::operator=(const ClapTrap &src)
 {
+	std::cout << "Copy assignement operator used." << std::endl;
 	this->_name = src._name;
 	this->_hitPoints = src._hitPoints;
 	this->_energyPoints = src._energyPoints;
 	this->_attackDamage = src._attackDamage;
+	std::cout << "Data for new object:" << std::endl;
+	std::cout << _name << std::endl;
+	std::cout << _hitPoints << std::endl;
+	std::cout << _energyPoints << std::endl;
+	std::cout << _attackDamage << std::endl;
 	return *this;
 }
 
 void	ClapTrap::attack(const std::string& target)
 {
-	if (_hitPoints > 1 || _energyPoints > 1)
+	if (_hitPoints < 1 || _energyPoints < 1)
 	{
 		std::cout << _name << "can´t attack. Is too wasted" << std::endl;
 		return ;
@@ -67,7 +74,7 @@ void	ClapTrap::takeDamage(unsigned int amount)
 
 void	ClapTrap::beRepaired(unsigned int amount)
 {
-	if (_hitPoints > 1 || _energyPoints > 1)
+	if (_hitPoints < 1 || _energyPoints < 1)
 	{
 		std::cout << _name << "can´t attack. Is too wasted" << std::endl;
 		return ;
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gprada-t <gprada-t@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/21 10:31:16 by gprada-t          #+#    #+#             */
-/*   Updated: 2024/07/21 12:44:32 by gprada-t         ###   ########.fr       */
+/*   Updated: 2024/07/21 13:04:02 by gprada-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,20 @@
 
 int	main(void)
 {
-	std::string name = "Louis";
-	ClapTrap testOne(name);
-	ClapTrap testTwo(testOne);
-	testOne.attack(name);
+	std::string nameOne = "Louis";
+	std::string nameTwo = "Charlie";
+
+	ClapTrap testOne(nameOne);
+	ClapTrap testTwo(nameTwo);
+
+	testOne.attack(nameTwo);
+	testTwo.takeDamage(2);
+	testTwo.beRepaired(10);
+
+	ClapTrap testThree(testTwo);
+
+	testOne = testThree;
+
 
 	return 0;
 }
